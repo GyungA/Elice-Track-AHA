01c5bb8802e74db5e67100a3d6b7dcfc2ac4cab6
KzeroJun <rladudwns164@gmail.com>
2024-04-02 12:05:33 +0900

    refactor : 불필요한 필드 삭제

--- src/main/java/com/secondproject/shoppingproject/user/dto/request/SignUpRequest.java
+++ src/main/java/com/secondproject/shoppingproject/user/dto/request/SignUpRequest.java
@@ -13,28 +13,25 @@ import lombok.*;
 @AllArgsConstructor
 public class SignUpRequest {
 
-    @NotBlank
-    @Pattern(regexp = "^[0-9A-Za-z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$")
-    String email;
+	@NotBlank
+	@Pattern(regexp = "^[0-9A-Za-z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$")
+	String email;
 
-    @NotBlank
+	@NotBlank
 //    @Size(min=8, max=20)
-    String password;
+	String password;
 
-    @NotBlank
-    String name;
+	@NotBlank
+	String name;
 
-    @NotBlank
-    @Size(min=7, max=7)
-    String birthdate;//생년월일 6자리와 주민 번호 앞자리 1개를 받음 => ex) 9806101
+	@NotBlank
+	@Size(min = 7, max = 7)
+	String birthdate;//생년월일 6자리와 주민 번호 앞자리 1개를 받음 => ex) 9806101
 
-    @NotBlank
-    boolean status;
+	@NotBlank
+	String address;
 
-    @NotBlank
-    String address;
-
-    @NotBlank
-    String phone;
+	@NotBlank
+	String phone;
 
 }
