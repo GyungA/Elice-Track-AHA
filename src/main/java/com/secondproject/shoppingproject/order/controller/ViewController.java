44c0e0e9e8b049d9dc7821e1098e032652beec21
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2024-04-18 21:42:17 +0900

    css, js 문제

--- src/main/java/com/secondproject/shoppingproject/order/controller/ViewController.java
+++ src/main/java/com/secondproject/shoppingproject/order/controller/ViewController.java
@@ -1,36 +1,38 @@
 package com.secondproject.shoppingproject.order.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 
-@RestController
-@RequestMapping("/order")
+@Controller
 public class ViewController {
-    @GetMapping //결제창
+    @GetMapping ("/order/pay")//결제창
     public String orderPage(Model model){
-        return "order/order";
+        return "order";
     }
 
-    @GetMapping("/mypage-order-detail") //마이페이지:주문 상세
+    @GetMapping("/order/detail") //마이페이지:주문 상세
     public String mypageOrderDatail(Model model){
-        return "mypage-order-detail/mypage-order-detail";
+        return "mypage-order-detail";
     }
 
-    @GetMapping("/mypage-order-detail") //마이페이지:주문 상세
-    public String mypageOrderDatail(Model model){
-        return "mypage-order-detail/mypage-order-detail";
+    @GetMapping("/order/management") //마이페이지:주문 관리
+    public String mypageOrderManagement(Model model){
+        return "mypage-order-management";
     }
 
-//    @GetMapping //결제창
-//    public String orderPage(Model model){
-//        return "order/order";
-//    }
-//
-//    @GetMapping //결제창
-//    public String orderPage(Model model){
-//        return "order/order";
-//    }
+    @GetMapping("/seller/order/management") //판매자: 주문 관리
+    public String sellerOrderManagement(Model model){
+        return "seller-order-management";
+    }
+
+    @GetMapping("/seller/order/detail") //판매자: 주문 상세
+    public String sellerOrderDetail(Model model){
+        return "seller-order-detail";
+    }
+
+
 
 }
