ef2cb8d9aec003ddfade237916ad5c704d47a1f2
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2024-04-18 18:58:10 +0900

    mypage 주문 관리 페이지

--- src/main/java/com/secondproject/shoppingproject/order/controller/ViewController.java
+++ src/main/java/com/secondproject/shoppingproject/order/controller/ViewController.java
@@ -5,27 +5,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 
-//@RestController
-//@RequestMapping("/order")
-//public class ViewController {
+@RestController
+@RequestMapping("/order")
+public class ViewController {
+    @GetMapping //결제창
+    public String orderPage(Model model){
+        return "order/order";
+    }
+
+    @GetMapping("/mypage-order-detail") //마이페이지:주문 상세
+    public String mypageOrderDatail(Model model){
+        return "mypage-order-detail/mypage-order-detail";
+    }
+
+    @GetMapping("/mypage-order-detail") //마이페이지:주문 상세
+    public String mypageOrderDatail(Model model){
+        return "mypage-order-detail/mypage-order-detail";
+    }
+
 //    @GetMapping //결제창
 //    public String orderPage(Model model){
 //        return "order/order";
 //    }
 //
-//    @GetMapping("/mypage-order-detail") //결제창
-//    public String mypageOrderDatail(Model model){
-//        return "mypage-order-detail/mypage-order-detail";
+//    @GetMapping //결제창
+//    public String orderPage(Model model){
+//        return "order/order";
 //    }
-//
-////    @GetMapping //결제창
-////    public String orderPage(Model model){
-////        return "order/order";
-////    }
-////
-////    @GetMapping //결제창
-////    public String orderPage(Model model){
-////        return "order/order";
-////    }
-//
-//}
+
+}
